0a569314af521b1069bd5d3530b5f2eb778176bb
alextorrer <alextorre0427@gmail.com>
2019-01-31 15:01:36 -0600

    Create suma_primos.cs

---
+++ suma_primos.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+        int x = 2;
+        int sum = 0;
+        Console.WriteLine("Ingrese n");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        while(n > 1){
+
+            if(n%x == 0){
+                n = n/x;
+                sum = sum + x;
+            }
+            else
+                x = x + 1;
+        }
+        sum = sum  + 1;
+        Console.WriteLine(sum);
+      }
+   }
+}
